5dcd275689c7ea4a3b90c4f0e170b69149063ce0
Giovanna Oliveira <giovannabeathryce.pso@gmail.com>
2023-04-24 11:55:17 -0300

    feat: finalized menu style preview

--- trakto_edu/src/app/app.module.ts
+++ trakto_edu/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
