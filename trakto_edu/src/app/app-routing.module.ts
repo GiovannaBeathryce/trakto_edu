923ce7e947a14ace9dcdf8c640ba16ccb30ae140
Giovanna Oliveira <giovannabeathryce.pso@gmail.com>
2023-04-25 13:15:42 -0300

    feat: preview finished material-page layout

--- trakto_edu/src/app/app-routing.module.ts
+++ trakto_edu/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MaterialDidaticoComponent } from './components/material-didatico/material-didatico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'material_didatico',
+    component: MaterialDidaticoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
